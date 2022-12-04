f5d9f0ea1cc890a2af04b12703e034c89770fe75
SUJITH_T_S <99735954+2002SUJITH@users.noreply.github.com>
2022-12-05 00:58:08 +0530

    palindrome check

--- strpalindrome.java
+++ strpalindrome.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-
 public class StrPalindrome {
 	public static void main(String[] args) {
 		Scanner in =new Scanner(System.in);
 		System.out.print("Enter the string : ");
 		String str=in.nextLine();
 		int len=str.length();
-        boolean flag=true;
+        	boolean flag=true;
 		for(int i=0;i<len/2;i++)
 			if( str.charAt(i)!= str.charAt(len-i-1)){ 
 				flag=false;
