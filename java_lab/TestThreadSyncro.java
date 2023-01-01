c527a0dd130ea16ca2401b3e955e52e74d754442
SUJITH_T_S <99735954+2002SUJITH@users.noreply.github.com>
2023-01-01 14:34:47 +0530

    thread synchroization

--- java_lab/TestThreadSyncro.java
+++ java_lab/TestThreadSyncro.java
@@ -1,12 +1,3 @@
-class TestThreadSyncro{
-	public static void main(String args[]){
-		Display d =new Display();
-		SyncThread t1 = new SyncThread(d,"Hello");
-		SyncThread t2 = new SyncThread(d,"World");
-		t1.start();
-		t2.start();
-	}
-}
 class Display{
 	public synchronized void print(String msg){
 		System.out.print("["+ msg);
@@ -31,8 +22,35 @@ class SyncThread extends Thread{
 		d.print(msg);	
 	}	
 } 
+class TestThreadSyncro{
+	public static void main(String args[]){
+		Display d =new Display();
+		SyncThread t1 = new SyncThread(d,"Hello");
+		SyncThread t2 = new SyncThread(d,"World");
+		t1.start();
+		t2.start();
+	}
+}
+
 
 /*
+output
+
 [Hello]
 [World]
+Algorithm 
+
+1. Create a 'Disply' class that contain a synchronised 'print()' method that takes a string as an 
+   argument 
+2. In the print() method print the string with brackets around it.
+3. sleep the thread for 2 seconds.
+4. catch if any exception thrown by the sleep() method and print the exception message.
+5. Create a SyncThread class that extends the Tread class and takes a Display object and string as 
+   constructor arguments
+6. In the run() method of SyncThread class ,call the print method of Display object with string as
+   argument
+7. In the main() method of TestThreadSynchro class create new Display object and  two 
+   SyncThread objects with the Display object and different strings as arguments
+8. Start both of the objects of SyncThread class(threads)
+
 */
